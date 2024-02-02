d89e14080cb4f0a9a362bb5a45c8dfdde67f8cdd
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2024-02-02 11:16:08 +0000

    Day 1 Part 2

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -10,10 +10,23 @@ let ExtractNumberFromString (input : string) : int =
     input
     |> String.filter Char.IsDigit
     |> ExtractNumber
-    |> int    
-    
+    |> int
+
+let ReplaceWordWithDigit(input : string) : string =
+    input
+        .Replace("one", "on1e")
+        .Replace("two", "tw2o")
+        .Replace("three", "thre3e")
+        .Replace("four", "fou4r")
+        .Replace("five", "fiv5e")
+        .Replace("six", "si6x")
+        .Replace("seven", "seve7n")
+        .Replace("eight", "eigh8t")
+        .Replace("nine", "nin9e")
+           
 let Day1 (input : string) : int =
     input.Split "\n"
+    |> Array.map ReplaceWordWithDigit
     |> Array.map ExtractNumberFromString
     |> Array.sum
     
--- Day1Test/Tests.fs
+++ Day1Test/Tests.fs
@@ -5,7 +5,13 @@ open Program
 open FluentAssertions
 
 [<Fact>]
-let ``Sample`` () =
+let ``Part1Test`` () =
     let input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
     let output = Day1 input
     output.Should().Be(142, "because that is the expected output")
+    
+[<Fact>]
+let ``Part2Test``() =
+    let input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
+    let output = Day1 input
+    output.Should().Be(281, "because that is the expected output")
