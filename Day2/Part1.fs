f5d60eb6170250324df81e8b875d0a135685b30f
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2024-02-06 11:09:13 +0000

    Day2 Part 2

--- Day2/Part1.fs
+++ Day2/Part1.fs
@@ -1,6 +1,5 @@
-﻿module Day2
+﻿module Day2Part1
 
-open System.IO
 open System.Text.RegularExpressions
 
 let maxRed = 12
@@ -11,11 +10,10 @@ let RemoveGame(game : string) =
     Regex.Replace(game, @"Game \d+: ", "")
 
 let MapGames(games : string array) : (int * string) array =
-    let a = games
-            |> Array.map _.Trim() 
-            |> Array.map _.Split(" ")
-            |> Array.map (fun game -> (game[0] |> int, game[1]))
-    a
+    games
+    |> Array.map _.Trim() 
+    |> Array.map _.Split(" ")
+    |> Array.map (fun game -> (game[0] |> int, game[1]))
 
 let IsGameValid game : bool =
     let count, colour = game
@@ -41,12 +39,9 @@ let Filter (game : int * bool) =
     | _, v when not v -> 0
     | _, _ -> failwith "Invalid value"
     
-let Day2(input : string) : int =
+let Day2Part1(input : string) : int =
     input.Split "\n"
     |> Array.map ProcessLine
     |> Array.indexed
     |> Array.map Filter
     |> Array.sum
-    
-let input = File.ReadAllText("input.txt");
-printfn $"{Day2(input)}"
---
+++ Day2/Part2.fs
@@ -0,0 +1,46 @@
+﻿module Day2Part2
+
+open System.IO
+open System.Text.RegularExpressions
+
+let RemoveGame(game : string) =
+    Regex.Replace(game, @"Game \d+: ", "")
+
+let MapGames(games : string array) : (int * string) array =
+    games
+    |> Array.map _.Trim() 
+    |> Array.map _.Split(" ")
+    |> Array.map (fun game -> (game[0] |> int, game[1]))
+
+let Split(text : string) =
+    text.Split [| ';'; ',' |]
+
+let Max (a, b) =
+    if fst a > fst b then
+        a
+    else
+        b
+        
+let GroupColour(pair, name) =
+    pair
+    |> Array.where (fun colour -> snd colour = name)
+    |> Array.map fst
+    |> Array.max
+    
+let GroupColours colours =
+    [ GroupColour(colours, "red"); GroupColour(colours, "blue"); GroupColour(colours, "green")]
+
+let ProcessLine text =
+    RemoveGame text
+    |> Split
+    |> MapGames
+    |> GroupColours
+    |> List.reduce (fun p c -> p * c)
+    
+let Day2Part2(input : string) : int =
+    input.Split "\n"
+    |> Array.map ProcessLine
+    |> Array.sum
+    
+let input = File.ReadAllText("input.txt");
+printfn $"{Day2Part2(input)}"
